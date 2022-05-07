b30e04e1c0b58c2d0e66e16eb3add69e6201db17
Adidi <adieloz@gmail.com>
2022-05-07 21:48:25 +0300

    setup

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -17,4 +17,13 @@ alias ntw="sudo service NetworkManager restart"
 
 alias gfap="git fetch --all --prune"
 
+alias lol="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'"
+
+alias lola="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --all"
+
+alias ss="git status -s"
+
+alias c="git commit -v"
+
+
 
---
+++ setup.sh
@@ -0,0 +1,9 @@
+
+git clone git@github.com:Adidi/.files.git ~/.files
+
+# oh my zsh
+ln -s ~/.files/oh-my-zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
+ln -s ~/.files/oh-my-zsh/into.zsh ~/.oh-my-zsh/custom/into.zsh
+
+# tmux
+ln -s ~/.files/tmux/.tmux.conf ~/.tmux.conf
