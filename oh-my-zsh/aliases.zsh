95ec5a7527389362bd14e51b682ae82412f7debc
Adidi <adieloz@gmail.com>
2022-05-07 21:23:02 +0300

    first commit

---
+++ oh-my-zsh/aliases.zsh
@@ -0,0 +1,20 @@
+
+alias ccb="xsel -ib <"
+
+# support unicode chars always
+alias tmux="tmux -u"
+
+# tmux new session with name
+alias txns="tmux new-session -s"
+
+# tmux join session by name
+alias txjs="tmux a -t"
+
+# create main tmux
+alias txm="txns main"
+
+alias ntw="sudo service NetworkManager restart"
+
+alias gfap="git fetch --all --prune"
+
+
